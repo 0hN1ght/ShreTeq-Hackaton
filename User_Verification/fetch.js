e0f5bab588063766560b8f25a4d9b6e6377ac4f7
Vihaan Vashistha <vihaanvashisthaa@gmail.com>
2024-10-20 00:00:56 +0530

    first commit

---
+++ User_Verification/fetch.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Firebase from "firebase";
+
+function Fetch(){
+
+    return(
+        <div>
+            <h1>Fetching Data</h1>
+        </div>
+    )
+}
+export default Fetch;
